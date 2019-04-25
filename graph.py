3da42990fd06c6cac598df2aff4ff4bf0bcbdf83
Anish Arvind Patil <32412360+Anish1207@users.noreply.github.com>
2019-04-25 12:11:37 +0530

    4 connected only

--- graph.py
+++ graph.py
@@ -63,28 +63,6 @@ def addNodeToGraph(graph, id, neighbors, edge=1):
 def makeGraph():
 	graph = {}
 
-	# 8-connected graph (w diagonals)
-	# Impossible to find path - 2 obstacles in middle
-	# graph = addNodeToGraph(graph, 'x1y1', ['x1y2', 'x2y1', 'x2y2'])
-	# graph = addNodeToGraph(
-	#     graph, 'x2y1', ['x1y1', 'x1y2', 'x3y1', 'x2y2', 'x3y2'], float('inf'))
-	# graph = addNodeToGraph(graph, 'x1y2', ['x1y1', 'x2y1', 'x2y2'])
-	# graph = addNodeToGraph(
-	#     graph, 'x2y2', ['x1y1', 'x1y2', 'x3y1', 'x2y1', 'x3y2'], float('inf'))
-	# graph = addNodeToGraph(graph, 'x3y1', ['x3y2', 'x2y1', 'x2y2'])
-	# graph = addNodeToGraph(graph, 'x3y2', ['x3y1', 'x2y1', 'x2y2'])
-
-	# 8-connected graph (w diagonals)
-	# Impossible to find path - 2 obstacles in middle
-	# graph = addNodeToGraph(graph, 'x1y1', ['x1y2', 'x2y1', 'x2y2'])
-	# graph = addNodeToGraph(
-	#     graph, 'x2y1', ['x1y1', 'x1y2', 'x3y1', 'x2y2', 'x3y2'], float('inf'))
-	# graph = addNodeToGraph(graph, 'x1y2', ['x1y1', 'x2y1', 'x2y2'])
-	# graph = addNodeToGraph(
-	#     graph, 'x2y2', ['x1y1', 'x1y2', 'x3y1', 'x2y1', 'x3y2'])
-	# graph = addNodeToGraph(graph, 'x3y1', ['x3y2', 'x2y1', 'x2y2'])
-	# graph = addNodeToGraph(graph, 'x3y2', ['x3y1', 'x2y1', 'x2y2'])
-
 	# 4-connected graph (w/out diagonals)
 	graph = addNodeToGraph(graph, 'x1y1', ['x1y2', 'x2y1'])
 	graph = addNodeToGraph(graph, 'x2y1', ['x1y1', 'x3y1', 'x2y2'])
